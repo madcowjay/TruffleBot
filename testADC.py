28bf76cbd1bab3f2ca578e91fd1072511238317c
Jason Webster <madcowjay@gmail.com>
2018-04-12 21:01:32 +0000

    voltage in testADC

--- testADC.py
+++ testADC.py
@@ -10,14 +10,14 @@ print('ADS1256 ID = ' + hex(myid))
 ads.ConfigADC()
 ads.SyncAndWakeup()
 
-ref_voltage = 4.5
+ref_voltage = 2.5
 
 # sample ADC
-ads.SetInputMux(ads.MUX_AIN0,ads.MUX_AINCOM)
+ads.SetInputMux(ads.MUX_AIN2,ads.MUX_AINCOM)
 ads.SyncAndWakeup()
 while True:
-	result = (ads.ReadADC())
-	percentage = float(result)/(2**24)
-	voltage = percentage*2*ref_voltage-ref_voltage
-	print('Result: %d, Percentage: %.2f, Voltage: %.5f' %(result, percentage, voltage))
+	result_in_twos_comp = (ads.ReadADC())
+	result = -(result_in_twos_comp & 0x800000) | (result_in_twos_comp & 0x7fffff)
+	voltage = (result*2*ref_voltage) / (2**23 - 1)
+	print('Result_in_twos_comp: %x, Result in decimal: %d, Voltage: %.5f' %(result_in_twos_comp, result, voltage))
 	time.sleep(.2)
