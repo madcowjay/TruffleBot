cb657c4fc0712848fe7bb1157a5a5a910046d578
Jason Webster <madcowjay@gmail.com>
2018-04-18 20:37:45 +0000

    testADC.py now has correct algorithm

--- testADC.py
+++ testADC.py
@@ -1,5 +1,16 @@
 import drivers.pyads1256
+import drivers.pylps22hb
 import time
+import wiringpi as wp
+import numpy as np
+
+wp.wiringPiSetupPhys
+wp.pinMode(26, wp.INPUT)
+
+all_cs = [33, 32, 40, 22, 35, 36, 7, 18]
+for cs in all_cs:
+	wp.pinMode(cs, wp.OUTPUT)
+	wp.digitalWrite(cs, wp.HIGH)
 
 # setup ADC
 ads = drivers.pyads1256.ADS1256()
@@ -11,13 +22,20 @@ ads.ConfigADC()
 ads.SyncAndWakeup()
 
 ref_voltage = 2.5
+gain = 2
 
-# sample ADC
-ads.SetInputMux(ads.MUX_AIN2,ads.MUX_AINCOM)
+ads.SetInputMux(ads.MUX_AIN1, ads.MUX_AINCOM)
 ads.SyncAndWakeup()
-while True:
-	result_in_twos_comp = (ads.ReadADC())
-	result = -(result_in_twos_comp & 0x800000) | (result_in_twos_comp & 0x7fffff)
-	voltage = (result*2*ref_voltage) / (2**23 - 1)
-	print('Result_in_twos_comp: %x, Result in decimal: %d, Voltage: %.5f' %(result_in_twos_comp, result, voltage))
-	time.sleep(.2)
+
+def read(n):
+	i = n
+	while i:
+		result_in_twos_comp = ads.ReadADC()
+		result = -(result_in_twos_comp & 0x800000) | (result_in_twos_comp & 0x7fffff)
+		voltage = (result*2*ref_voltage) / (2**23 - 1) / gain
+		print('Result_in_twos_comp: %x, Result in decimal: %d, Voltage: %.5f' %(result_in_twos_comp, result, voltage))
+		i -= 1
+		res = int(result_in_twos_comp)
+		res = float(res)
+		#print('other way: ' + str(((res-2**23) % 2**24) / 2**24))
+		print('np way:    ' + str(np.mod(res-2**23,2**24)/2**24))
