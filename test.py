97628a55e6289b02ff0d5c1ba18c7180ddc95ac6
Jason Webster <madcowjay@gmail.com>
2018-04-19 13:37:08 -0400

    typo fixed

--- test.py
+++ test.py
@@ -43,7 +43,7 @@ def read_once():
 	result = -(result_in_twos_comp & 0x800000) | (result_in_twos_comp & 0x7fffff)
 	voltage = (result*2*ref_voltage) / (2**23 - 1) / gain
 	res = float(result_in_twos_comp)
-	perc = np.mod(res-2**23,2**24)/2**24)
+	perc = np.mod(res-2**23,2**24)/2**24
 	print('Voltage: %.9f, percent of range: %.9f' %(voltage, perc))
 
 def read(n):
--- testADC.py
+++ testADC.py
@@ -33,6 +33,6 @@ def read(n):
 		result = -(result_in_twos_comp & 0x800000) | (result_in_twos_comp & 0x7fffff)
 		voltage = (result*2*ref_voltage) / (2**23 - 1) / gain
 		res = float(result_in_twos_comp)
-		perc = np.mod(res-2**23,2**24)/2**24)
+		perc = np.mod(res-2**23,2**24)/2**24
 		print('Voltage: %.9f, percent of range: %.9f' %(voltage, perc))
 		n -= 1
