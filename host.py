a6a11192fe185d63a6cbf2e7e5813acecb119eaa
Jason Webster <madcowjay@gmail.com>
2018-05-07 14:33:17 -0400

    'bugs'

--- host.py
+++ host.py
@@ -3,14 +3,14 @@ Updated 2018/05/03
   -Implemented config files for experiment parameters and IPs of clients
 
   -kill_processes now takes a client file as an argument
-     and is called at the beginning of the program to clean up
-     any running copies that might be leftover from crashes.
+	 and is called at the beginning of the program to clean up
+	 any running copies that might be leftover from crashes.
   -moved ip_list to host.py and made it an argument to PiManager
   -Unthreaded listener
   -Python3 only supported now
   -datavisualiztion removed
   -board numbers removed (use serial number instead)
-                                    -JW
+									-JW
 """
 
 import os, sys, platform, time, textwrap
@@ -24,34 +24,34 @@ configFlag = False
 # Process command line arguments
 index = 0
 for arg in sys.argv[1:]:
-    index += 1
-    if arg == '-d' or arg == '--debug':
-        os.environ['DEBUG'] = 'True'
-    elif arg == '-c' or arg == '--config-file':
-        configFlag = True
-        configFilePath = sys.argv[index+1]
-    elif arg[0:14] == '--config-file=':
-        configFlag = True
-        configFilePath = arg[14:]
-    elif arg == '-r' or arg == '--remote-install':
-        remoteInstallFlag = True
-    elif arg == '--help':
-        print('Usage: python3 host.py [OPTION]...')
-        print('  -d, --debug                         display debug messages while running')
-        print('  -r, --remote-install                install all necessary files on clients')
-        print('  -c, --config-file=CONFIG.CFG        use the indicated configuration file, if not invoked, default.cfg is used')
-        sys.exit()
-    else:
-        print("host.py: invalid option -- '{0}'".format(arg[1:]))
-        print("Try 'host.py --help' for more information.")
-        sys.exit()
+	index += 1
+	if arg == '-d' or arg == '--debug':
+		os.environ['DEBUG'] = 'True'
+	elif arg == '-c' or arg == '--config-file':
+		configFlag = True
+		configFilePath = sys.argv[index+1]
+	elif arg[0:14] == '--config-file=':
+		configFlag = True
+		configFilePath = arg[14:]
+	elif arg == '-r' or arg == '--remote-install':
+		remoteInstallFlag = True
+	elif arg == '--help':
+		print('Usage: python3 host.py [OPTION]...')
+		print('  -d, --debug                         display debug messages while running')
+		print('  -r, --remote-install                install all necessary files on clients')
+		print('  -c, --config-file=CONFIG.CFG        use the indicated configuration file, if not invoked, default.cfg is used')
+		sys.exit()
+	else:
+		print("host.py: invalid option -- '{0}'".format(arg[1:]))
+		print("Try 'host.py --help' for more information.")
+		sys.exit()
 
 # Load these after DEBUG status has been determined
 import lib.savefile
 import lib.connect
 
 if not configFlag:
-    configFilePath = 'default.cfg'
+	configFilePath = 'default.cfg'
 config = configparser.RawConfigParser()
 config.read(configFilePath)
 print('\nLoading config file: ' + configFilePath)
@@ -96,8 +96,8 @@ print('    ip addresses: ' , end = '')
 print(*ip_list, sep = ', ')
 
 if remoteInstallFlag:
-    print('TODO')
-    # sync files, run client on the remote machines
+	print('TODO')
+	# sync files, run client on the remote machines
 #host_dir='pi_utils'
 host_dir    = config.get('paths', 'host_dir')
 client_dir  = config.get('paths', 'client_dir')
@@ -157,39 +157,39 @@ s.settimeout(5.0)
 #                 break
 #             else:
 #                 tx[n*tx_bit_len +i] = 0
-    #
-    #
-    # #tweak the message into a form that can be saved and reconstructed easily
-    # #this essentially translates the message into an array constrained by the samplerate of the Sensors
-    # msg_plot = []
-    # element_len = int(pulse_duration*samplerate)
-    # for element in message:
-    #     for n in range(element_len):
-    #         msg_plot.append(element)
-    # msg_plot = np.pad(msg_plot,samplerate*padding,'constant',constant_values=0)
-    # print(msg_plot)
-    # print(msg_plot.shape)
-    #
-    # return message,tx,msg_plot
+	#
+	#
+	# #tweak the message into a form that can be saved and reconstructed easily
+	# #this essentially translates the message into an array constrained by the samplerate of the Sensors
+	# msg_plot = []
+	# element_len = int(pulse_duration*samplerate)
+	# for element in message:
+	#     for n in range(element_len):
+	#         msg_plot.append(element)
+	# msg_plot = np.pad(msg_plot,samplerate*padding,'constant',constant_values=0)
+	# print(msg_plot)
+	# print(msg_plot.shape)
+	#
+	# return message,tx,msg_plot
 
 #== Main Code ==================================================================
 for trial in range(iterations): # number of times to do experiment
-    # set start time of experiment
-    print('\n*** trial %s started ***' %(trial))
-    pe.set_start_time()
+	# set start time of experiment
+	print('\n*** trial %s started ***' %(trial))
+	pe.set_start_time()
 
 #============================================================================
-    #manage the pis
-    #pm.conditional_dir_sync()
-    #pm.upload_file(client_file)
-    #pm.exec_command('sudo pkill python')
+	#manage the pis
+	#pm.conditional_dir_sync()
+	#pm.upload_file(client_file)
+	#pm.exec_command('sudo pkill python')
 
-    #sanitize client-directory
-    pm.exec_commands(['rm %s/sendfile.pickle'%pm.client_dir,'rm %s/txpattern.pickle'%pm.client_dir])
+	#sanitize client-directory
+	pm.exec_commands(['rm %s/sendfile.pickle'%pm.client_dir,'rm %s/txpattern.pickle'%pm.client_dir])
 
-    #get identifying dictionaries from pm
-    ip_serial = pm.identifpi() #-JW
-    print('board list: ' + str(ip_serial)) #-JW
+	#get identifying dictionaries from pm
+	ip_serial = pm.identifpi() #-JW
+	print('board list: ' + str(ip_serial)) #-JW
 
 #########################################################################
 # # transmit section
@@ -213,102 +213,102 @@ for trial in range(iterations): # number of times to do experiment
 #########################################################################
 
 #============================================================================
-    ## start client script
-    pm.run_script(client_file, log_file)
-    # pm.exec_command('sudo strace -p $(pgrep python) -o strace.txt')
-    time.sleep(4) # wait for remote programs to start
+	## start client script
+	pm.run_script(client_file, log_file)
+	# pm.exec_command('sudo strace -p $(pgrep python) -o strace.txt')
+	time.sleep(4) # wait for remote programs to start
 
-    # add sensors to experiment
-    responses_requested = 0
-    for ip in pm.ip_list:
-        pe.add_sensor('Board #'+str(ip_serial[ip]),samplerate=samplerate)
-        pe.add_sensor_parameter('Board #'+str(ip_serial[ip]),'Serial Number',str(ip_serial[ip]))
-        pe.add_sensor_parameter('Board #'+str(ip_serial[ip]),'Type','MOX')
-        responses_requested += 1
+	# add sensors to experiment
+	responses_requested = 0
+	for ip in pm.ip_list:
+		pe.add_sensor('Board #'+str(ip_serial[ip]),samplerate=samplerate)
+		pe.add_sensor_parameter('Board #'+str(ip_serial[ip]),'Serial Number',str(ip_serial[ip]))
+		pe.add_sensor_parameter('Board #'+str(ip_serial[ip]),'Type','MOX')
+		responses_requested += 1
 
-    #kill the program if no sensors respond
-    if not responses_requested:
-        print('There were no boards in the ip_list - Exiting')
-        sys.exit()
+	#kill the program if no sensors respond
+	if not responses_requested:
+		print('There were no boards in the ip_list - Exiting')
+		sys.exit()
 
-    #==========================================================================================================
+	#==========================================================================================================
 
-    #send command to the client to collect data
-    pulse_duration = 1; #-JW
-    command = 'collect %s %s %s %s'%(num_samples, spacing, pulse_duration, padding)
-    s.sendto(command.encode('utf-8'), dest)
-    print('sending command: ' + command)
+	#send command to the client to collect data
+	pulse_duration = 1; #-JW
+	command = 'collect %s %s %s %s'%(num_samples, spacing, pulse_duration, padding)
+	s.sendto(command.encode('utf-8'), dest)
+	print('sending command: ' + command)
 
-    #start lsitening until all responses are in
-    start_time = time.time()
-    print(time.strftime('%H:%M:%S',time.localtime(start_time)) + ' : listener started')
-    s.settimeout(1)#shorter timeout for recieving to work in long loop+
-    responses_received = 0
-    while responses_received < responses_requested:
-        curr_time = time.time() - start_time
-        try:
-            (buf, address) = s.recvfrom(8192)
-            response = buf.decode('utf-8')
-            if response!='unknown':
-                if response == 'end_flag':
-                    responses_received += 1
-                    print('    {0:>4} : received response from: {1}'.format(int(curr_time), address))
-                    print('    {0:>4} : total responses: {1}'.format(int(curr_time), responses_received))
-                else: pass
-        except Exception as e:
-            print('    {0:>4} : {1}'.format(int(curr_time), e))
-    print('    {0:>4} : listener ended'.format(int(curr_time)))
+	#start lsitening until all responses are in
+	start_time = time.time()
+	print(time.strftime('%H:%M:%S',time.localtime(start_time)) + ' : listener started')
+	s.settimeout(1)#shorter timeout for recieving to work in long loop+
+	responses_received = 0
+	while responses_received < responses_requested:
+		curr_time = time.time() - start_time
+		try:
+			(buf, address) = s.recvfrom(8192)
+			response = buf.decode('utf-8')
+			if response!='unknown':
+				if response == 'end_flag':
+					responses_received += 1
+					print('    {0:>4} : received response from: {1}'.format(int(curr_time), address))
+					print('    {0:>4} : total responses: {1}'.format(int(curr_time), responses_received))
+				else: pass
+		except Exception as e:
+			print('    {0:>4} : {1}'.format(int(curr_time), e))
+	print('    {0:>4} : listener ended'.format(int(curr_time)))
 
-    #end experiment
-    pe.set_end_time()
+	#end experiment
+	pe.set_end_time()
 
-    #==========================================================================================================
+	#==========================================================================================================
 
-    # get data from pis, reassemble data
-    data = {}
-    sample_times = {}
-    for ip in pm.ip_list:
-        pm.ssh.connect(ip, username, password)
-        sftp = pm.ssh.open_sftp()
-        with tempfile.TemporaryFile() as fp:
-            sftp.getfo('/home/pi/TruffleBot/log/sendfile.pickle',fp)
-            fp.seek(0)
-            log = pickle.load(fp,encoding='latin1') #incompatibility of np arrays between python 2(clients) and 3(host) so use latin1 encoding
-            data[ip] = log
-        print('TxPattern: ' + str(data[ip]['TxPattern']))
+	# get data from pis, reassemble data
+	data = {}
+	sample_times = {}
+	for ip in pm.ip_list:
+		pm.ssh.connect(ip, username, password)
+		sftp = pm.ssh.open_sftp()
+		with tempfile.TemporaryFile() as fp:
+			sftp.getfo('/home/pi/TruffleBot/log/sendfile.pickle',fp)
+			fp.seek(0)
+			log = pickle.load(fp,encoding='latin1') #incompatibility of np arrays between python 2(clients) and 3(host) so use latin1 encoding
+			data[ip] = log
+		print('TxPattern: ' + str(data[ip]['TxPattern']))
 
-    #save data in log file, scale the data
-    for board in pe.sensors.keys():
-        print(board)
-        serial = board[7:]
-        # print('  >board serial: ' + str(serial))
-        ip = ip_serial.inv[serial]
-        ret_data = data[ip]['Data']
-        savedata = ret_data.astype('float32')
-        #scale data to reference 0 = 2**23
-        for n in np.nditer(savedata, op_flags=['readwrite']):
-             n[...] = np.mod(n-2**23,2**24)/2**24
-        print('    >data :\n' + textwrap.indent(str(savedata), '          '))
-        pe.add_data(board,savedata)
-        pe.add_sensor_parameter(board,'End Time',data[ip]['End Time'])
-        print('    >end time: %s, avg elapse: %s'%(data[ip]['End Time'],data[ip]['Average Elapsed']))
+	#save data in log file, scale the data
+	for board in pe.sensors.keys():
+		print(board)
+		serial = board[7:]
+		# print('  >board serial: ' + str(serial))
+		ip = ip_serial.inv[serial]
+		ret_data = data[ip]['Data']
+		savedata = ret_data.astype('float32')
+		#scale data to reference 0 = 2**23
+		for n in np.nditer(savedata, op_flags=['readwrite']):
+			 n[...] = np.mod(n-2**23,2**24)/2**24
+		print('    >data :\n' + textwrap.indent(str(savedata), '          '))
+		pe.add_data(board,savedata)
+		pe.add_sensor_parameter(board,'End Time',data[ip]['End Time'])
+		print('    >end time: %s, avg elapse: %s'%(data[ip]['End Time'],data[ip]['Average Elapsed']))
 
-    #==========================================================================================================
-    #kill processes on remote machines
-    pm.kill_processes(client_file)
+	#==========================================================================================================
+	#kill processes on remote machines
+	pm.kill_processes(client_file)
 
-    # add number of sources, sensors to experiment parameters
-    pe.set_parameter('# Sensors',responses_received)
-    pe.set_parameter('# Sources',1)
-    pe.set_parameter('Wind Speed (m/s)', 2.1)
+	# add number of sources, sensors to experiment parameters
+	pe.set_parameter('# Sensors',responses_received)
+	pe.set_parameter('# Sources',1)
+	pe.set_parameter('Wind Speed (m/s)', 2.1)
 
-    # save log
-    try:
-        log_path, date_time = pl.save_file(pe)
-    except Exception as e:
-        print('error'+str(e))
+	# save log
+	try:
+		log_path, date_time = pl.save_file(pe)
+	except Exception as e:
+		print('error'+str(e))
 
-    #visualize returned data
-    # logname = log_path.split('/')[1]
-    # print('visualizing')
-    # dv.visualize(logname)
+	#visualize returned data
+	# logname = log_path.split('/')[1]
+	# print('visualizing')
+	# dv.visualize(logname)
--- lib/connect.py
+++ lib/connect.py
@@ -3,118 +3,132 @@ Updated 2018/05/02
   -kill_processes now takes a file instead of an ip list. It will kill all copies
    of that program on all ips in PiManager's ip list that were launched from
    connect.py (by matching 'python3 -u <client_file>')
-                                    -JW
+									-JW
 Updated 2018/05/01
   -moved ip_list to host.py and made it an argument to PiManager
-                                    -JW
+									-JW
 
 Updated 2018/04/28
   -log file includes errors now
   -python3 called with -u to flush output
   -log file back to appending
-                                    -JW
+									-JW
 
 Updated 2018/04/25
   -File/Directory synchronizing removed for now
   -ip addresses hard coded
   -board numbers removed (use serial number)
   -log file overwrites now instead of appending
-                                    -JW
+									-JW
 """
 
-import os, subprocess, time, re
+import os, subprocess, time, re, select
 import paramiko, bidict
 from   lib.debug_print import *
 
 
 class PiManager:
-    def __init__(self, client_dir, ip_list):
-        #init global variables
-        self.client_dir = client_dir
-        self.ip_list = ip_list
+	def __init__(self, client_dir, ip_list):
+		#init global variables
+		self.client_dir = client_dir
+		self.ip_list = ip_list
 
-        #init paramiko
-        self.ssh = paramiko.SSHClient()
-        self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+		debug_print('PiManager initializing with:')
+		debug_print('   client_dir      = {}'.format(self.client_dir))
+		debug_print('   ip_list         = {}'.format(self.ip_list))
 
-
-    def identifpi(self):
-        ip_serial=bidict.bidict()
-        #get serial number for each pi, tie to ip address
-        for ip in self.ip_list:
-            self.ssh.connect(ip, username='pi', password='raspberryB1oE3')
-            stdin,stdout,stderr = self.ssh.exec_command("cat /proc/cpuinfo | grep Serial | cut -d ' ' -f 2")
-            ip_serial[ip] = stdout.read().decode().replace('\n','')
-        self.ssh.close()
-        return ip_serial
+		#init paramiko
+		self.ssh = paramiko.SSHClient()
+		self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 
-    #executes a command on each client
-    def exec_command(self, command, addr=None):
-        try:
-            if addr:
-                self.ssh.connect(addr, username='pi', password='raspberryB1oE3')
-                self.ssh.exec_command(command)
-                debug_print('%s: executed "%s"'%(addr,command))
-                self.ssh.close()
-            else:
-                for ip in self.ip_list:
-                    self.ssh.connect(ip, username='pi', password='raspberryB1oE3')
-                    self.ssh.exec_command(command)
-                    debug_print('%s: executed "%s"'%(ip,command))
-                self.ssh.close()
-        except Exception as e:
-            print(e)
+	def identifpi(self):
+		debug_print('identifpi started')
+		ip_serial=bidict.bidict()
+		#get serial number for each pi, tie to ip address
+		for ip in self.ip_list:
+			self.ssh.connect(ip, username='pi', password='raspberryB1oE3')
+			command = "cat /proc/cpuinfo | grep Serial | cut -d ' ' -f 2"
+			stdin,stdout,stderr = self.ssh.exec_command(command)
+			ip_serial[ip] = stdout.read().decode().replace('\n','')
+		self.ssh.close()
+		debug_print('ip_serial: {}'.format(ip_serial))
+		return ip_serial
 
 
-    #executes multiple commands on each client
-    def exec_commands(self, commands, addr=None):
-        try:
-            if addr:
-                for command in commands:
-                    self.ssh.connect(addr, username='pi', password='raspberryB1oE3')
-                    self.ssh.exec_command(command)
-                    debug_print('%s: executed "%s"'%(addr,command))
-                self.ssh.close()
-            else:
-                for ip in self.ip_list:
-                    for command in commands:
-                        self.ssh.connect(ip, username='pi', password='raspberryB1oE3')
-                        self.ssh.exec_command(command)
-                        debug_print('%s: executed "%s"'%(ip,command))
-                    self.ssh.close()
-        except Exception as e:
-            print(e)
+	#executes a command on each client
+	def exec_command(self, command, addr=None):
+		debug_print('exec_command started')
+		try:
+			if addr:
+				self.ssh.connect(addr, username='pi', password='raspberryB1oE3')
+				self.ssh.exec_command(command)
+				debug_print('%s: executed "%s"'%(addr,command))
+				self.ssh.close()
+			else:
+				for ip in self.ip_list:
+					self.ssh.connect(ip, username='pi', password='raspberryB1oE3')
+					self.ssh.exec_command(command)
+					debug_print('%s: executed "%s"'%(ip,command))
+				self.ssh.close()
+		except Exception as e:
+			print(e)
 
 
-    #takes the file to to run as argument, and runs it on each client as sudo
-    def run_script(self, client_file, log_file=None):
-        try:
-            for ip in self.ip_list:
-                self.ssh.connect(ip, username='pi', password='raspberryB1oE3')
-                if log_file:
-                    self.ssh.exec_command('cd %s && python3 -u %s &>> log/%s'%(self.client_dir,client_file,log_file))
-                    debug_print('%s: starting client file, writing stdout and stderr to %s'%(ip,log_file))
-                else:
-                    self.ssh.exec_command('cd %s && python3 %s'%(self.client_dir,client_file))
-                    debug_print('%s: starting client file'%ip)
-            self.ssh.close()
-        except Exception as e:
-            print(e)
+	#executes multiple commands on each client
+	def exec_commands(self, commands, addr=None):
+		debug_print('exec_commands started')
+		try:
+			if addr:
+				for command in commands:
+					self.ssh.connect(addr, username='pi', password='raspberryB1oE3')
+					self.ssh.exec_command(command)
+					debug_print('%s: executed "%s"'%(addr,command))
+				self.ssh.close()
+			else:
+				for ip in self.ip_list:
+					for command in commands:
+						self.ssh.connect(ip, username='pi', password='raspberryB1oE3')
+						self.ssh.exec_command(command)
+						debug_print('%s: executed "%s"'%(ip,command))
+					self.ssh.close()
+		except Exception as e:
+			print(e)
 
 
-    #takes a dictionary of form: ip:PID and iterates through it, killing each PID
-    def kill_processes(self, client_file):
-        killed = {}
-        try:
-            for ip in self.ip_list:
-                self.ssh.connect(ip, username='pi', password='raspberryB1oE3')
-                stdin,stdout,stderr = self.ssh.exec_command('pgrep -f "python3 -u %s"'%(client_file))
-                pids = stdout.read().decode()
-                for pid in pids.split('\n'):
-                    if pid != '':
-                        self.ssh.exec_command('sudo kill %s'%pid)
-                        debug_print('%s: killed "%s"'%(ip,pid))
-            self.ssh.close()
-        except Exception as e:
-            print(e)
+	#takes the file to to run as argument, and runs it on each client as sudo
+	def run_script(self, client_file, log_file=None):
+		debug_print('run_script started')
+		try:
+			for ip in self.ip_list:
+				self.ssh.connect(ip, username='pi', password='raspberryB1oE3')
+				if log_file:
+					command = 'cd %s && python3 -u %s &>> log/%s'%(self.client_dir,client_file,log_file)
+					debug_print(command)
+					stdin, stdout, stderr = self.ssh.exec_command(command)
+					debug_print('%s: starting client file, writing stdout and stderr to %s'%(ip,log_file))
+				else:
+					command = 'cd %s && python3 %s'%(self.client_dir,client_file)
+					self.ssh.exec_command(command)
+					debug_print('%s: starting client file'%ip)
+			self.ssh.close()
+		except Exception as e:
+			print(e)
+
+
+	#takes a dictionary of form: ip:PID and iterates through it, killing each PID
+	def kill_processes(self, client_file):
+		debug_print('kill_processes started')
+		killed = {}
+		try:
+			for ip in self.ip_list:
+				self.ssh.connect(ip, username='pi', password='raspberryB1oE3')
+				stdin,stdout,stderr = self.ssh.exec_command('pgrep -f "python3 -u %s"'%(client_file))
+				pids = stdout.read().decode()
+				for pid in pids.split('\n'):
+					if pid != '':
+						self.ssh.exec_command('sudo kill %s'%pid)
+						debug_print('%s: killed "%s"'%(ip,pid))
+			self.ssh.close()
+		except Exception as e:
+			print(e)
