1f9ab079157cfbb5350cba1e830cbe9ab2ba0f15
Jason Webster <madcowjay@gmail.com>
2018-05-29 15:59:42 -0400

    bug

--- client.py
+++ client.py
@@ -1,4 +1,4 @@
-import os, sys, time, socket, pickle, threading, configparser
+import os, sys, time, socket, pickle, threading, configparser, ast
 import numpy as np
 import wiringpi as wp
 from   multiprocessing import Queue
@@ -70,11 +70,11 @@ include_press_temp = config.getboolean('include-sensor-types', 'press/temp')
 
 
 channel  = int(config.get('pulser', 'channel'))
-port     = config.get('pulser', 'port')
+port     = ast.liter_eval(config.get('pulser', 'port'))
 baudrate = int(config.get('pulser', 'baudrate'))
-parity   = config.get('pulser', 'parity')
-stopbits = config.get('pulser', 'stopbits')
-bytesize = config.get('pulser', 'bytesize')
+parity   = ast.literal_eval(config.get('pulser', 'parity'))
+stopbits = ast.literal_eval(config.get('pulser', 'stopbits'))
+bytesize = ast.literal_eval(config.get('pulser', 'bytesize'))
 
 sb = lib.sensor_board.SENSOR_BOARD(LED1_PIN, LED2_PIN, TX0_PIN, TX1_PIN)
 sb.ledAct(1,0) # turn them both off to start
